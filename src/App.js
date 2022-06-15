f2e4cb0a5af5606ee82d8869d5bcda6c452e8d3e
nehakikani <nehakikani23@gmail.com>
2022-06-15 13:20:57 +0530

    pages

--- src/App.js
+++ src/App.js
@@ -3,12 +3,27 @@ import './App.css';
 import Home from './Container/home/Home';
 import Footer from './Component/footer/Footer';
 import Header from './Component/header/Header';
+import Department from './Container/departments/Departments';
+import Doctors from './Container/doctors/Doctors';
+import About from './Container/about/About';
+import Contact from './Container/contact/Contact';
+import {Route,Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home/>
+        <Switch>
+            <Route path={'/'} exact component={Home}></Route>
+            <Route path={'/Departments'} exact component={Department}></Route>
+            <Route path={'/Doctors'} exact component={Doctors}></Route>
+            <Route path={'/About'} exact component={About}></Route>
+            <Route path={'/Contact'} exact component={Contact}></Route>
+
+
+
+        </Switch>
+      {/* <Home/> */}
       <Footer/>
     </div>
   );
